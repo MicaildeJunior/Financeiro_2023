5a01496b5e8d68da405696c27dd3e258b4d29df9
MicaildeJunior <micaoliveira.tecnologo@gmail.com>
2023-03-30 20:28:24 -0300

    Repositorio Completo

--- Domain/Interfaces/IUsuarioSistemaFinanceiro/InterfaceUsuarioSistemaFinanceiro.cs
+++ Domain/Interfaces/IUsuarioSistemaFinanceiro/InterfaceUsuarioSistemaFinanceiro.cs
@@ -1,4 +1,6 @@
 ﻿
+using Domain.Interfaces.Generics;
+using Entities.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +9,11 @@ using System.Threading.Tasks;
 
 namespace Domain.Interfaces.IUsuarioSistemaFinanceiro;
 
-public interface InterfaceUsuarioSistemaFinanceiro
+public interface InterfaceUsuarioSistemaFinanceiro : InterfaceGeneric<UsuarioSistemaFinanceiro>
 {
+    Task<IList<UsuarioSistemaFinanceiro>> ListarUsuarioSistema(int IdSistema);
+
+    Task RemoveUsuarios(List<UsuarioSistemaFinanceiro> usuario);
+
+    Task<UsuarioSistemaFinanceiro> ObterUsuarioPorEmail(string emailUsuario);
 }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,12 @@
+using Domain.Interfaces.Generics;
+using Domain.Interfaces.ICategoria;
+using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.ISistemaFinanceiro;
+using Domain.Interfaces.IUsuarioSistemaFinanceiro;
 using Entities.Entidades;
 using Infra.Configuracao;
+using Infra.Repositorio;
+using Infra.Repositorio.Generics;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,11 +18,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// CONFIGURAÇÃO DO BANCO DE DADOS 
 builder.Services.AddDbContext<ContextBase>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ContextBase>();
+
+
+// INTERFACE E REPOSITORIO
+builder.Services.AddSingleton(typeof(InterfaceGeneric<>), typeof(RepositoryGenerics<>));
+builder.Services.AddSingleton<InterfaceCategoria, RepositorioCategoria>();
+builder.Services.AddSingleton<InterfaceDespesa, RepositorioDespesa>();
+builder.Services.AddSingleton<InterfaceSistemaFinanceiro, RepositorioSistemaFinanceiro>();
+builder.Services.AddSingleton<InterfaceUsuarioSistemaFinanceiro, RepositorioUsuarioSistemaFinanceiro>();
 
 
 var app = builder.Build();
